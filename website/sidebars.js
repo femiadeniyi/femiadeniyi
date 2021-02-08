726439d4ab09722a03244433043d7672d6dbdb84
femi adeniyi <femi@webdeveloper.shop>
2021-02-08 17:20:14 +0000

    chore(WDS-4): commit ...rest

---
+++ website/sidebars.js
@@ -0,0 +1,16 @@
+module.exports = {
+    someSidebar: {
+        "Getting Started": [
+            'doc1', 'doc2', 'doc3'
+        ],
+        Development: [
+            "setting-up"
+        ],
+        Styling: [],
+        Building: [],
+        Backend:[],
+        Deploy:[],
+        Advanced:[],
+        Features: ['mdx'],
+    },
+};
