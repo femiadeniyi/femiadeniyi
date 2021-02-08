a9fe1078ab5f906545d909e68fd63b78d469499c
femi adeniyi <femi@webdeveloper.shop>
2021-02-08 17:32:58 +0000

    chore(WDS-4): republish

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,24 +1,32 @@
 import fs from "fs-extra"
 import tsconfig from "./tsconfig.json"
 
-function readPackageJson() {
-    const packageJson = fs.readJSONSync("./package.json")
-    return packageJson
-}
 
-function readTsConfig(){
-    const packageJson = fs.readJSONSync("./tsconfig.json")
-    console.log(packageJson)
-}
 
 function setTsConfig(){
+    console.log("setting tsconfig.json")
     fs.writeJSONSync("tsconfig.json",tsconfig,{spaces:2})
+    console.log("set tsconfig.json")
 }
 
 function setPackageJson(){
-    const packageJson = readPackageJson()
-    fs.writeJSON("package.json",{...packageJson, main:"dist/index.js"},{spaces:4})
+    console.log("setting package.json")
+    const packageJson = fs.readJSONSync("./package.json")
+    const newPackageJson = {
+        ...packageJson,
+        main:"dist/index.js",
+        private:false,
+    }
+    if(packageJson.private || packageJson.main !== "dist/main.js"){
+        fs.writeJSON("package.json",{...packageJson, main:"dist/index.js"},{spaces:2})
+    }
+    console.log("set package.json")
+    return packageJson
 }
 
+function main(){
+    setPackageJson()
+    setTsConfig()
+}
 
-setTsConfig()
+main()
