215b2265a76567e4e260478d2353bcf76829f9ca
femi adeniyi <lilses@live.co.uk>
2021-03-29 01:04:51 +0100

    feat(ci pipeline): experimental

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "github_actions_secret" "example_secret" {
 resource "github_actions_secret" "google" {
   repository = "femiadeniyi"
   secret_name = "GOOGLE_APPLICATION_CREDENTIALS"
-  plaintext_value = base64encode(var.SERVICE_ACCOUNT)
+  plaintext_value = var.SERVICE_ACCOUNT
 }
 
 resource "github_actions_secret" "docker-username" {
